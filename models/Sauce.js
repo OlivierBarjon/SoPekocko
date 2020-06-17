4f10bf71338895012d4148328314aa425eeaa558
Olivier Barjon <olivier.barjon@gmail.com>
2020-06-17 13:31:22 +0200

    création du modèle User

--- models/Sauce.js
+++ models/Sauce.js
@@ -7,11 +7,11 @@ const sauceSchema = mongoose.Schema({
     description:{type:String, required:true}, // description de la sauce
     mainPepper:{type:String, required:true}, // principal ingrédient dans la sauce
     imageUrl:{type:String, required:true}, // url de l'image de la sauce téléchargée par l'utilisateur
-    heat: {type:Number, required:true}, // note de 1 à 10
-    likes: {type:Number, required:true}, // nombre d'utilisateur qui aiment la sauce
-    dislikes: {type:Number, required:true}, //nombre d'utilisateur qui n'aiment pas la sauce
-    usersLiked: {type:String, required:true}, // tableau d'id d'utilisateur ayant aimé la sauce
-    usersDisliked: {type:String, required:true}// tableau d'id d'utilisateur n'ayant pas aimé la sauce
+    heat: {type:Number}, // note de 1 à 10
+    likes: {type:Number}, // nombre d'utilisateur qui aiment la sauce
+    dislikes: {type:Number}, //nombre d'utilisateur qui n'aiment pas la sauce
+    usersLiked: {type:String}, // tableau d'id d'utilisateur ayant aimé la sauce
+    usersDisliked: {type:String}// tableau d'id d'utilisateur n'ayant pas aimé la sauce
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose'); // récupération de mongoose
+const uniqueValidator = require('mongoose-unique-validator'); // récupération du package gérant la propriété "unique" afin d'éviter d'avoir plusieurs utilisateurs avec la même adresse mail
 
 const userSchema = mongoose.Schema({
     userId:{type:String, required:true},
-    email:{type:String, required:true},
+    email:{type:String, required:true, unique:true},
     password:{type:String, required:true}
 
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
