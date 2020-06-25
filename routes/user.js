ddf1d6164561752bf8061808867a17a29a57b9ef
Olivier Barjon <olivier.barjon@gmail.com>
2020-06-25 10:18:19 +0200

    installation express-bouncer

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express'); // on récupère express afin de pouvoir créer un routeur
 const router = express.Router(); // on crée un routeur
 const userCtrl = require('../controllers/user'); // on récupère le controleur pour user
+const bouncer = require('express-bouncer')(500, 30000, 5); //on récupère express-bouncer (anti attaque par force brut) et on le paramètre pour qu'un utilisateur ne puisse pas saisir plus de 5 mots de passes consécutivement sans devoir attendre entre 0.5s et 30secondes pour retenter de se connecter
+bouncer.whitelist.push ("127.0.0.1"); // ajout de localhost sur liste blanche
+
 
 /* ### ROUTES ### */
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block, userCtrl.login);
 
 /* EXPORT */
 module.exports = router;
